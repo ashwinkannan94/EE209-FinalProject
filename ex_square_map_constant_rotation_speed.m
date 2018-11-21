4327df7671962c5622c92004a00c9fda503d7e79
Chris Baek <chris@Chriss-MacBook-Pro-3.local>
2018-11-20 18:39:30 -0800

    ex_square_map_constant_rotation_speed.m now uses two rangefinders rotating together.

--- ex_square_map_constant_rotation_speed.m
+++ ex_square_map_constant_rotation_speed.m
@@ -1,14 +1,27 @@
 [environment, env_size_x, env_size_y] = get_environment_from_image('square_map.png'); % load map from PNG
 
-% Robot's position
-robot_pos_x = 500;
-robot_pos_y = 500;
+% Robot's position (slight near the upper right corner)
+robot_pos_x = 550;
+robot_pos_y = 550;
+
+% Sensor rotation speed
+rotation_speed = 1; % degree per sample
 
 % get sensor data time series for two 360 degree turns
-sensor_data = [];
-for theta = 1:720
-    measurement = get_rangefinder_distance(robot_pos_x, robot_pos_y, theta, environment, env_size_x, env_size_y);
-    sensor_data = [sensor_data, measurement]; % append current sensor measurement
+front_sensor_data = [];
+right_sensor_data = [];
+
+theta = 1;
+while theta < 720
+    front_measurement = get_rangefinder_distance(robot_pos_x, robot_pos_y, theta, environment, env_size_x, env_size_y);
+    front_sensor_data = [front_sensor_data, front_measurement]; % append current sensor measurement
+    
+    right_measurement = get_rangefinder_distance(robot_pos_x, robot_pos_y, theta-90, environment, env_size_x, env_size_y);
+    right_sensor_data = [right_sensor_data, right_measurement]; % append current sensor measurement
+    
+    % random rotation speed
+%     rotation_speed = randi(30); % between 1 and 10 degree per sample
+    theta = theta + rotation_speed;
 end
 
 figure;
@@ -21,6 +34,12 @@ ylim([0 env_size_y]);
 title('Environment');
 
 figure;
-plot(sensor_data);
+subplot(2,1,1);
+plot(front_sensor_data);
 xlabel('time');
-ylabel('sensor measurement');
+ylabel('front sensor measurement');
+
+subplot(2,1,2); 
+plot(right_sensor_data);
+xlabel('time');
+ylabel('right sensor measurement');
