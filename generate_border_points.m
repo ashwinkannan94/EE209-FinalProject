c30417929b057a6e61e3c7c1013ebbbf77892e22
Ashwin Kannan <ashwinkumar.kannan@gmail.com>
2018-11-28 02:38:21 -0800

    doc strings

--- generate_border_points.m
+++ generate_border_points.m
@@ -1,11 +1,19 @@
 function [x_border_positions,y_border_positions] = generate_border_points(distance, degrees, starting_position_x, starting_position_y)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+% Generates border points based on distance and degrees data
+%   Using the degrees and the distance that is provided, find new x and y
+%   coordinates which will be where the border is. Basic logic is to use
+%   the following:
+%   x_border = starting_x + distance*sin(degree)
+%   y_border = starting_y + distance*cos(degree)
+
     x_border_positions = [];
     y_border_positions = [];
     for idx = 1:length(distance)
         x_border_positions = [x_border_positions, starting_position_x + distance(idx)*sind(degrees(idx))];
-        y_border_positions = [y_border_positions, starting_position_y + distance(idx)*cosd(degrees(idx))];
+        x_border_positions = [y_border_positions, starting_position_y + distance(idx)*cosd(degrees(idx))];
     end
+%   figure
+%   scatter(x_border_positions,y_border_positions)
+%   Do these two lines above where the code is called
 end
 
