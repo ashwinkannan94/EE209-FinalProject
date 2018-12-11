885aabd8d1ac99f6a06e1731d916bec95fecdbc0
bweezy <bwood94@ucla.edu>
2018-12-11 15:57:07 -0800

    added optimizations to get_sensor_data

--- get_sensor_data.m
+++ get_sensor_data.m
@@ -3,27 +3,25 @@ function [front_sensor_data, right_sensor_data] = get_sensor_data(robot_pos_x, r
     rotation_speed = 0.1;
     
     [environment, env_size_x, env_size_y] = env{:};
+    
+    front_sensor_data = zeros(1,360*1/rotation_speed);
 
-    front_sensor_data = [];
-    right_sensor_data = [];
-
-    theta = 0;
-
-    while theta < 360 % just one turn
+    parfor theta1 = 1:(360/rotation_speed) % just one turn
+        theta = theta1*rotation_speed;
         if mod(theta,30) == 0 % print theta for every 30 degrees
             disp(theta);
         end
         front_measurement = get_rangefinder_distance(robot_pos_x, robot_pos_y, theta, environment, env_size_x, env_size_y);
-        front_sensor_data = [front_sensor_data, front_measurement]; % append current sensor measurement
+        front_sensor_data(theta1) = front_measurement; % append current sensor measurement
 
-        right_measurement = get_rangefinder_distance(robot_pos_x, robot_pos_y, theta-20, environment, env_size_x, env_size_y);
-        right_sensor_data = [right_sensor_data, right_measurement]; % append current sensor measurement
+        %right_measurement = get_rangefinder_distance(robot_pos_x, robot_pos_y, theta-20, environment, env_size_x, env_size_y);
+        %right_sensor_data = [right_sensor_data, right_measurement]; % append current sensor measurement
 
         % random rotation speed
     %     rotation_speed = randi(30); % between 1 and 10 degree per sample
-        theta = theta + rotation_speed;
+        %theta = theta + rotation_speed;
     end
-    
+    right_sensor_data = [front_sensor_data(end-199:end),front_sensor_data(1:end-200)];
     
     front_sensor_data = [front_sensor_data, front_sensor_data, front_sensor_data, front_sensor_data, front_sensor_data, front_sensor_data];
     right_sensor_data = [right_sensor_data, right_sensor_data, right_sensor_data, right_sensor_data, right_sensor_data, right_sensor_data];
